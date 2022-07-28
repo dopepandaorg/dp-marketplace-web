3bfe4a2872685128d6c8853aed4c689431e208bd
DopePanda Dev <dopepandaorg@protonmail.com>
2022-07-28 09:37:55 +0530

    Past and active contests display

--- src/routes/contests/index.svelte
+++ src/routes/contests/index.svelte
@@ -7,8 +7,26 @@
 	import ContestTilePlaceholder from '$lib/components/contest/ContestTilePlaceholder.svelte'
 	import { InlineNotification } from 'carbon-components-svelte'
 
+	let isLoading = false
+	let activeContests = []
+	let pastContests = []
+
 	const contests = operationStore<any>(Q_GET_CONTESTS)
 	query(contests)
+
+	contests.subscribe(cq => {
+		isLoading = cq.fetching
+
+		if (cq.data && cq.data.contests) {
+			activeContests = cq.data.contests
+				.filter(c => new Date(c.end_at) >= new Date())
+				.sort((a, b) => new Date(b.start_at).getTime() - new Date(a.start_at).getTime())
+			
+			pastContests = cq.data.contests
+				.filter(c => new Date(c.end_at) < new Date())
+				.sort((a, b) => new Date(b.start_at).getTime() - new Date(a.start_at).getTime())
+		}
+	})
 </script>
 
 <svelte:head>
@@ -36,20 +54,44 @@
 				hideCloseButton
 			/>
 		{:else}
-			<div class="contests__list">
-				{#each $contests.data.contests as contest (contest.id)}
-					<ContestTile {contest} />
-				{/each}
-			</div>
+			{#if activeContests.length > 0}
+				<h2>Active Contests</h2>
+				
+				<div class="contests__list">
+					{#each activeContests as contest (contest.id)}
+						<ContestTile {contest} />
+					{/each}
+				</div>
+			{/if}
+
+			{#if pastContests.length > 0}
+				<h2>Past Contests</h2>
+				
+				<div class="contests__list">
+					{#each pastContests as contest (contest.id)}
+						<ContestTile {contest} />
+					{/each}
+				</div>
+			{/if}
 		{/if}
 	</div>
 </div>
 
 <style lang="scss">
+	h2 {
+		font-weight: bold;
+		margin-bottom: 1.5rem;
+	}
+
 	.contests__list {
 		display: grid;
 		grid-template-columns: 1fr;
 		gap: 2rem;
+		margin-bottom: 4rem;
+
+		&:last-of-type {
+			margin-bottom: 0;
+		}
 
 		@media screen and (min-width: 768px) {
 			grid-template-columns: 1fr 1fr;
